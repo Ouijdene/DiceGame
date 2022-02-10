f8e86c49f70038b6195e86fd0c443ac8d7b35948
Ouijdene <ouijdene.derouiche@gmail.com>
2022-02-10 21:31:36 +0100

    Add a button

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-//First Dice
+function diceGame() {
+  //First Dice
 
-var randomNumber1 = Math.floor(Math.random()*6)+1;
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDice1= "images/dice" + randomNumber1 + ".png"; 
+  var randomDice1 = "images/dice" + randomNumber1 + ".png";
 
-document.querySelectorAll("img")[0].setAttribute("src", randomDice1);
+  document.querySelectorAll("img")[0].setAttribute("src", randomDice1);
 
-//Second Dice
-var randomNumber2 = Math.floor(Math.random()*6)+1;
+  //Second Dice
 
-var randomDice2= "images/dice" + randomNumber2 + ".png"; 
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-document.querySelectorAll("img")[1].setAttribute("src", randomDice2);
+  var randomDice2 = "images/dice" + randomNumber2 + ".png";
 
-//Result
+  document.querySelectorAll("img")[1].setAttribute("src", randomDice2);
 
-if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML="Player 1 wins";
+  //Result
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
 }
-else if (randomNumber1 < randomNumber2){
-    document.querySelector("h1").innerHTML="Player 2 wins";
-}
-else{
-    document.querySelector("h1").innerHTML="Draw!";  
-}
+
+diceGame();
